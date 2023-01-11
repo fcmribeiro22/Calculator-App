391744bf4fb168ba7755b6b9fa7654d658056ee6
Fabio Ribeiro <fcmribeiro22@gmail.com>
2023-01-11 17:15:59 +0000

    Assemble the Calculator UI

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ function divide(operand1,operand2){
     return operand1 / operand2;
 };
 
-function operation(operation,operand1,operand2){
-    return operation(operand1,operand2);
+function operate(operation,operand1,operand2){
+    return operate(operand1,operand2);
 };
 
-console.log(operation(multiply,10,2));
