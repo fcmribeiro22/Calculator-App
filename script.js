072a332a75ecf2effb532fa7dfaea073ca147e61
Fabio Ribeiro <fcmribeiro22@gmail.com>
2023-01-13 19:50:05 +0000

    Give all the buttons values and concacenate every button after click.

--- script.js
+++ script.js
@@ -18,3 +18,76 @@ function operate(operation,operand1,operand2){
     return operate(operand1,operand2);
 };
 
+const display = document.querySelector('.display');
+const button = document.querySelectorAll('button');
+document.querySelector('.weird-button').disabled = true;
+document.querySelector('.percentage-button').disabled = true;
+
+let displayValue= 0;
+
+
+
+function displayAfterclick(){
+   if (display.innerHTML==="0"){
+    display.innerHTML ="";
+    display.innerHTML += this.value;
+    
+
+} else{
+    display.innerHTML += this.value;
+}
+displayValue= display.innerHTML;
+console.log(displayValue);
+}
+
+const toDisplay= () =>{
+    for(let i= 0; i<button.length; i++){
+            button[i].addEventListener('click',displayAfterclick);
+            
+    }
+    
+}
+
+
+
+toDisplay();
+
+
+
+
+
+
+
+
+// let displayValue = " ";
+
+// function updateDisplay(value) {
+//     if (display.innerHTML === "0") {
+//         display.innerHTML = value;
+//     } else {
+//         display.innerHTML += value;
+//     }
+// }
+
+// function updateDisplayValue() {
+//     displayValue = display.innerHTML;
+    
+// }
+
+// function displayAfterClick() {
+//     updateDisplay(this.value);
+//     updateDisplayValue();
+// }
+
+// const toDisplay = () => {
+//     for (let i = 0; i < button.length; i++) {
+//         button[i].addEventListener("click", displayAfterClick);
+//     }
+// };
+
+// toDisplay();
+
+
+
+
+
