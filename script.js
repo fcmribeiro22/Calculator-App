3bed2acc64ae7a6d3da9f8559b2a89910d474438
Fabio Ribeiro <fcmribeiro22@gmail.com>
2023-01-15 00:32:15 +0000

    Add equal functionality

--- script.js
+++ script.js
@@ -14,14 +14,21 @@ function divide(operand1,operand2){
     return operand1 / operand2;
 };
 
-function operate(operation,operand1,operand2){
-    return operation(operand1,operand2);
-};
+function operate(operator, operand1, operand2) {
+    switch (operator) {
+        case "+":
+            return add(operand1, operand2);
+        case "-":
+            return subtract(operand1, operand2);
+        case "*":
+            return multiply(operand1, operand2);
+        case "/":
+            return divide(operand1, operand2);
+        default:
+            return "Invalid operator";
+    }
 
-function test(){
-    console.log(operator);
 }
-
 
 
 const display = document.querySelector('.display');
@@ -30,8 +37,6 @@ const equalButton = document.querySelector('.equals');
 const clearButton = document.querySelector('.clear-button');
 const operatorButtons= document.querySelectorAll('.operator');
 
-document.querySelector('.weird-button').disabled = true;
-document.querySelector('.percentage-button').disabled = true;
 
 let displayValue= 0;
 let firstOperand ="";
@@ -39,34 +44,51 @@ let secondOperand ="";
 let operator= "";
 
 
+function clearScreen(){
 
-function displayAfterclick(){
-
-   if (display.innerHTML==="0"){
-    display.innerHTML ="";
-    display.innerHTML += this.value;
-    
-
-} else{
-    display.innerHTML += this.value;
+ 
+  secondOperand ="";
+  operator= "";
+  display.innerHTML="0";
 }
-displayValue= display.innerHTML;
 
-}
+
 
 function setOperator() {
     operator = this.value;
     
 };
 
-function getOperator(){
+function setOperatorAfterClick(){
     for(let i=0; i<operatorButtons.length; i++){
         operatorButtons[i].addEventListener('click',setOperator);
-            }
-            
+    }
+    
 }
 
 
+
+function displayAfterclick(){
+    if (operator === ""){
+        if (display.innerHTML === "0") {
+            display.innerHTML = "";
+        } 
+        display.innerHTML += this.value;
+        firstOperand = display.innerHTML;
+    } else {
+        if (display.innerHTML !== "0") {
+            display.innerHTML += this.value;
+            secondOperand = display.innerHTML.substring(firstOperand.length + 1);
+        } else {
+            firstOperand = "0";
+            display.innerHTML = "";
+            display.innerHTML += this.value;
+            secondOperand = display.innerHTML.substring(2);
+        }
+    }
+    console.log(firstOperand);
+    console.log(secondOperand);
+};
 
 const toDisplay= () =>{
     for(let i= 0; i<button.length; i++){
@@ -77,48 +99,14 @@ const toDisplay= () =>{
 }
 
 
+clearButton.addEventListener('click',clearScreen);
+equalButton.addEventListener('click', function() {
+    let result = operate(operator, Number(firstOperand), Number(secondOperand));
+    display.textContent = result;
+});
 
 
-getOperator();
+setOperatorAfterClick();
 toDisplay();
-
-
-
-
-
-
-
-
-
-// let displayValue = " ";
-
-// function updateDisplay(value) {
-//     if (display.innerHTML === "0") {
-//         display.innerHTML = value;
-//     } else {
-//         display.innerHTML += value;
-//     }
-// }
-
-// function updateDisplayValue() {
-//     displayValue = display.innerHTML;
-    
-// }
-
-// function displayAfterClick() {
-//     updateDisplay(this.value);
-//     updateDisplayValue();
-// }
-
-// const toDisplay = () => {
-//     for (let i = 0; i < button.length; i++) {
-//         button[i].addEventListener("click", displayAfterClick);
-//     }
-// };
-
-// toDisplay();
-
-
-
 
 
